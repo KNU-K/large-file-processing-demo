48f0a1f7a99317809047b0fc9f3a1dff5850f6bf
KNU-K <knukang334@gmail.com>
2024-10-11 02:07:53 +0900

    init :initialize spring boot application

---
+++ src/main/java/com/large_file_processing/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.large_file_processing.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/large_file_processing/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.large_file_processing.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
