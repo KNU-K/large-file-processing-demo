2e8b3724e546539c03e3fce370781c81dfa06452
KNU-K <knukang334@gmail.com>
2024-10-11 18:23:26 +0900

    feat : add fileController
    * config endpoint for uploading.

---
+++ src/main/java/com/large_file_processing/demo/controller/FileController.java
@@ -0,0 +1,30 @@
+package com.large_file_processing.demo.controller;
+
+import com.large_file_processing.demo.service.LocalFileUploadService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/file")
+public class FileController {
+    private final LocalFileUploadService localFileUploadService;
+    @PostMapping
+    public String upload(@RequestParam("file") MultipartFile file) {
+        log.info("Received file: {}", file.getOriginalFilename());
+        return localFileUploadService.upload(file);
+    }
+}
