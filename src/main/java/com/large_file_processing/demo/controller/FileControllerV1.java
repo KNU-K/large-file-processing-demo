2f57ab64da89f7356b09e672f61c0bbd2be91387
KNU-K <knukang334@gmail.com>
2024-10-12 05:50:51 +0900

    feat : add endpoints for chunk-based and generic file uploads

--- src/main/java/com/large_file_processing/demo/controller/FileControllerV1.java
+++ src/main/java/com/large_file_processing/demo/controller/FileControllerV1.java
@@ -1,6 +1,6 @@
 package com.large_file_processing.demo.controller;
 
-import com.large_file_processing.demo.service.LocalFileUploadService;
+import com.large_file_processing.demo.service.FileUploadService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,27 +9,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/file")
-public class FileController {
-    private final LocalFileUploadService localFileUploadService;
-    @PostMapping
-    public String upload( @RequestParam("file") MultipartFile file,
-                          @RequestParam("filename") String filename,
+public class FileControllerV1 {
+    private final FileUploadService fileUploadService;
+    @PostMapping("/chunk")
+    public String uploadChunk( @RequestParam("file") MultipartFile file,
                           @RequestParam("originalFilename") String originalFilename,
                           @RequestParam("chunkIndex") int chunkIndex,
                           @RequestParam("totalChunks") int totalChunks) {
         log.info("Received file: {}", originalFilename);
 
-        return localFileUploadService.upload(file, originalFilename, chunkIndex, totalChunks);
+        return fileUploadService.uploadChunk(file, originalFilename, chunkIndex, totalChunks);
+    }
+    @PostMapping()
+    public String upload( @RequestParam("file") MultipartFile file) {
+        log.info("Received file: {}", file.getOriginalFilename());
+
+        return fileUploadService.upload(file);
     }
 }
